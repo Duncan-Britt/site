5354039dbd2e173eba4e2ae9de7177ce6c85426c
Duncan Britt <dbru997@gmail.com>
2023-03-13 10:59:12 -0600

    New blog post about A* Search

--- public/blog/path-finding/path-finding-untangled.js
+++ public/blog/path-finding/path-finding-untangled.js
@@ -1,172 +1,152 @@
-let init_grid_str = `
-................
-................
-...##......##...
-....#.......#...
-....#.......#...
-....######..###.
-.........#......
-.........#......
-.........#......
-...##....####...
-....#.......#...
-....#.......#...
-....####....###.
-................
-................
-`.trim();
-
-const EMPTY = 0;
-const OCCUPIED = 1;
-const END_POINT = 2;
-
 class Grid {
   constructor(grid_str) {
     const rows = grid_str.split('\n');
     this.nRows = rows.length;
     this.nCols = rows[0].length;
-    this._grid = Array(this.nCols * this.nRows).fill(EMPTY);
+    this._grid = Array(this.nCols * this.nRows).fill(0);
     rows.forEach((row, y) => {
       row.split('').forEach((char, x) => {
-        this._grid[y * this.nCols + x] = char == '.' ? EMPTY : char == '#' ? OCCUPIED : END_POINT;
+        this._grid[y * this.nCols + x] = char == '.' ? 0 : char == '#' ? 1 : 2;
       });
     });
 
-    this.start_idx = this.nCols * (this.nRows - 1); // Traverse from the top right
-    this.end_idx = this.nCols - 1;                  // to the bottom left corner
-    this._grid[this.start_idx] = END_POINT;         
-    this._grid[this.end_idx] = END_POINT;
+    this.start_idx = this.nCols * (this.nRows - 1);
+    this.end_idx = this.nCols - 1;
+    this._grid[this.start_idx] = 2;
+    this._grid[this.end_idx] = 2;
     this.path_capacity = 100;
     this.path_size = 0;
     this.path = Array(this.path_capacity);
   }
 
-set_at(val, x, y) {
-  if (x < 0 || y < 0) {
-    return;
-  }
-  this._grid[y * this.nCols + x] = val;
-}
-
-at(x, y) {
-  return this._grid[y * this.nCols + x];
-}
-
-idx_to_cartesian(idx) {
-  return [idx % this.nCols, Math.floor(idx / this.nCols)];
-}
-
-distance(idx_a, idx_b) {
-  let [ ax, ay ] = this.idx_to_cartesian(idx_a);
-  let [ bx, by ] = this.idx_to_cartesian(idx_b);
-
-  return Math.round(10 * Math.sqrt(Math.pow(by - ay, 2) + Math.pow(bx - ax, 2)));
-}
-
-clear_path() {
-  this.path_size = 0;
-}
-
-plot_course() {
-  const pq_max_size = 10000;
-  let pq = Array(pq_max_size);
-  for (let i = 0; i < pq_max_size; i += 1) {
-    pq[i] = new PathNode();
+  clear_path() {
+    this.path_size = 0;
   }
 
-  pq[0].grid_idx = this.end_idx;
-  pq[0].parent = null;
-  pq[0].gCost = 0;
-  pq[0].hCost = this.distance(this.end_idx, this.start_idx);
-  let pq_size = 1;
-
-const explored_max_size = pq_max_size;
-let explored = Array(explored_max_size).fill(new PathNode());
-for (let i = 0; i < pq_max_size; i += 1) {
-  explored[i] = new PathNode();
-}
-let explored_size = 0;
-
-// 5 6 7
-// 4 X 0
-// 3 2 1
-const neighbors = [
-  1, this.nCols + 1, this.nCols, this.nCols - 1, -1,
-  (0 - this.nCols) - 1, (0 - this.nCols), (0 - this.nCols) + 1
-];
-
-while (pq_size != 0) {
-  if (explored_size >= explored_max_size) {
-    return -5;
+  set_at(val, x, y) {
+    if (x < 0 || y < 0) {
+      return;
+    }
+    this._grid[y * this.nCols + x] = val;
   }
-  pq_size = pq_dequeue(pq, pq_size, explored, explored_size);
-  explored_size += 1;
 
-  if (explored[explored_size-1].grid_idx == this.start_idx) {
-    this.path[0] = explored[explored_size-1].grid_idx;
-    this.path_size += 1;
-    let node = explored[explored_size - 1].parent;
-    for (let i = 1; node instanceof PathNode; i += 1) {
-      this.path[i] = node.grid_idx;
-      node = node.parent;
-      this.path_size += 1;
-      if (this.path_size == this.path_capacity && node instanceof PathNode) {
-        return -1; // Path capacity reached before completing path.
+  at(x, y) {
+    return this._grid[y * this.nCols + x];
+  }
+
+  idx_to_cartesian(idx) {
+    return [idx % this.nCols, Math.floor(idx / this.nCols)];
+  }
+
+  distance(idx_a, idx_b) {
+    let [ ax, ay ] = this.idx_to_cartesian(idx_a);
+    let [ bx, by ] = this.idx_to_cartesian(idx_b);
+
+    return Math.round(10 * Math.sqrt(Math.pow(Math.abs(by - ay), 2) + Math.pow(Math.abs(bx - ax), 2)));
+  }
+
+  plot_course() {
+    // 5 6 7
+    // 4 X 0
+    // 3 2 1
+    const neighbors = [
+      1, this.nCols + 1, this.nCols, this.nCols - 1, -1,
+      (0 - this.nCols) - 1, (0 - this.nCols), (0 - this.nCols) + 1
+    ];
+
+    const pq_max_size = 10000;
+    let pq = Array(pq_max_size);
+    for (let i = 0; i < pq_max_size; i += 1) {
+      pq[i] = new PathNode();
+    }
+
+    pq[0].grid_idx = this.end_idx;
+    pq[0].parent = null;
+    pq[0].gCost = 0;
+    pq[0].hCost = this.distance(this.end_idx, this.start_idx);
+    let pq_size = 1;
+
+    const explored_max_size = pq_max_size;
+    let explored = Array(explored_max_size).fill(new PathNode());
+    for (let i = 0; i < pq_max_size; i += 1) {
+      explored[i] = new PathNode();
+    }
+
+    let explored_size = 0;
+    while (pq_size != 0) {
+      if (explored_size >= explored_max_size) {
+        return -5;
+      }
+      pq_size = pq_dequeue(pq, pq_size, explored, explored_size);
+      explored_size += 1;
+
+      for (let i = 0; i < 8; i += 1) {
+        if (explored[explored_size-1].grid_idx < this.nCols && i >= 5) continue; // first row
+        if (explored[explored_size-1].grid_idx % this.nCols == 0 && i >= 3 && i <= 5) continue; // first column
+        if (explored[explored_size-1].grid_idx % this.nCols == this.nCols - 1 && (i <= 1 || i == 7)) continue; // last column
+        if (Math.floor(explored[explored_size-1].grid_idx / this.nCols) == this.nRows - 1 && (i <= 3 && i >= 1)) continue; // last row
+
+        let neighbor_idx = explored[explored_size - 1].grid_idx + neighbors[i];
+
+        if (this._grid[neighbor_idx] == 1) {
+          continue;
+        }
+
+        if (neighbor_idx == this.start_idx) {
+          this.path[0] = neighbor_idx;
+          this.path_size += 1;
+          let node = explored[explored_size - 1];
+          for (let i = 1; node instanceof PathNode; i += 1) {
+            this.path[i] = node.grid_idx;
+            node = node.parent;
+            this.path_size += 1;
+            if (this.path_size == this.path_capacity && node instanceof PathNode) {
+              return -1; // Path capacity reached before completing path.
+            }
+          }
+
+          return 0;
+        }
+
+        // ask whether the neighbor_idx exists in the pq already
+        let in_pq = false;
+        let pq_idx = 0;
+        for (let i = 0; i < pq_size; i += 1) {
+          if (pq[i].grid_idx == neighbor_idx) {
+            in_pq = true;
+            pq_idx = i;
+            break;
+          }
+        }
+        // If so, the gCost and parent may need to be updated to reflect a lower cost path
+        if (in_pq) {
+          let new_gCost = explored[explored_size - 1].gCost + this.distance(explored[explored_size - 1].grid_idx, neighbor_idx);
+          if (new_gCost < pq[pq_idx].gCost) {
+            pq[pq_idx].gCost = new_gCost;
+            pq[pq_idx].parent = explored[explored_size - 1];
+            pq_shift_up(pq, pq_idx);
+          }
+        } else { // otherwise, add a new PathNode to the queue
+          pq[pq_size].grid_idx = neighbor_idx;
+          pq[pq_size].parent = explored[explored_size - 1];
+          pq[pq_size].gCost = explored[explored_size - 1].gCost + this.distance(explored[explored_size - 1].grid_idx, neighbor_idx);
+          pq[pq_size].hCost = this.distance(neighbor_idx, this.start_idx);
+          pq_shift_up(pq, pq_size);
+          pq_size += 1;
+          // console.log(`pq_size: ${pq_size}, pq_max_size: ${pq_max_size}, pq.length: ${pq.length}`);
+          // console.log(pq);
+          if (pq_size == pq_max_size) {
+            return -4; // Reached capacity for priority queue
+          }
+        }       
       }
     }
 
-    return 0;
-  }
-
-for (let i = 0; i < 8; i += 1) {
-  if (explored[explored_size-1].grid_idx < this.nCols && i >= 5) continue; // first row
-  if (explored[explored_size-1].grid_idx % this.nCols == 0 && i >= 3 && i <= 5) continue; // first column
-  if (explored[explored_size-1].grid_idx % this.nCols == this.nCols - 1 && (i <= 1 || i == 7)) continue; // last column
-  if (Math.floor(explored[explored_size-1].grid_idx / this.nCols) == this.nRows - 1 && (i <= 3 && i >= 1)) continue; // last row
-
-  let neighbor_idx = explored[explored_size - 1].grid_idx + neighbors[i];
-
-if (this._grid[neighbor_idx] == OCCUPIED) {
-        continue;
-      }
-
-      // // ask whether the neighbor_idx exists in the pq already
-      let in_pq = false;
-      let pq_idx = 0;
-      for (let i = 0; i < pq_size; i += 1) {
-        if (pq[i].grid_idx == neighbor_idx) {
-          in_pq = true;
-          pq_idx = i;
-          break;
-        }
-      }
-      // If so, the gCost and parent may need to be updated to reflect a lower cost path
-      if (in_pq) {
-        let new_gCost = explored[explored_size - 1].gCost + this.distance(explored[explored_size - 1].grid_idx, neighbor_idx);
-        if (new_gCost < pq[pq_idx].gCost) {
-          pq[pq_idx].gCost = new_gCost;
-          pq[pq_idx].parent = explored[explored_size - 1];
-          pq_shift_up(pq, pq_idx);
-        }
-      } else { // otherwise, add a new PathNode to the queue
-        pq[pq_size].grid_idx = neighbor_idx;
-        pq[pq_size].parent = explored[explored_size - 1];
-        pq[pq_size].gCost = explored[explored_size - 1].gCost + this.distance(explored[explored_size - 1].grid_idx, neighbor_idx);
-        pq[pq_size].hCost = this.distance(neighbor_idx, this.start_idx);
-
-        pq_shift_up(pq, pq_size);
-        pq_size += 1;
-
-        if (pq_size == pq_max_size) {
-          return -4; // Reached capacity for priority queue
-        }
-      }       
-    }
-  }
-
-  return 1; // Path not found
+    return 1; // Path not found
   }
 }
+
+let SCALE = 18;
 
 class PathNode {
   constructor() {
@@ -195,18 +175,6 @@ function pq_left_child(i) {
 
 function pq_right_child(i) {
   return (2 * i) + 2;
-}
-
-function pq_shift_up(pq, i) {
-  while (i > 0 && pq[pq_parent(i)].greater_than(pq[i])) {
-    let parent_i = pq_parent(i);
-    let temp = new PathNode();
-    Object.assign(temp, pq[i]);
-    Object.assign(pq[i], pq[parent_i]);
-    Object.assign(pq[parent_i], temp);
-
-    i = parent_i;
-  }
 }
 
 function pq_shift_down(pq, size, i) {
@@ -244,7 +212,18 @@ function pq_dequeue(pq, pq_size, explored, explored_size) {
   return pq_size - 1;
 }
 
-const SCALE = 18;
+function pq_shift_up(pq, i) {
+  while (i > 0 && pq[pq_parent(i)].greater_than(pq[i])) {
+    let parent_i = pq_parent(i);
+    let temp = new PathNode();
+    Object.assign(temp, pq[i]);
+    Object.assign(pq[i], pq[parent_i]);
+    Object.assign(pq[parent_i], temp);
+
+    i = parent_i;
+  }
+}
+
 class CanvasDisplay {
   constructor(parent, grid) {
     this.canvas = document.createElement("canvas");
@@ -341,7 +320,7 @@ class CanvasDisplay {
       if (err == 0) {
         this.draw_path(this.grid);
       } else {
-        console.log(err);
+        // console.log(err);
       }
     }).bind(this);
 
@@ -359,7 +338,7 @@ class CanvasDisplay {
     if (err == 0) {
       this.draw_path(this.grid);
     } else {
-      console.log(err);
+      // console.log(err);
     }
   }
 
@@ -409,6 +388,24 @@ CanvasDisplay.prototype.draw_path = function({ path, path_size }) {
 
   this.cx.stroke();
 }
+
+let init_grid_str = `
+................
+................
+...##......##...
+....#.......#...
+....#.......#...
+....######..###.
+.........#......
+.........#......
+.........#......
+...##....####...
+....#.......#...
+....#.......#...
+....####....###.
+................
+................
+`.trim();
 
 let grid = new Grid(init_grid_str);
 var is_drawing = false;
